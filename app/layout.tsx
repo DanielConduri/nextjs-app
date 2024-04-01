f3684cd84a256b93d13d099a4d4b85388c23dc48
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-04-01 00:09:09 -0500

    ruta dashboard finalizada

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+  
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      
+      <body className={`${montserrat.className} antialiased`}>
+      {/* Esto es parte del layout */}
+      {/* <h1>Este titulo</h1>
+      <div style={{border: '1px solid #000'}}> */}
+      {/* el chilren es el page.tsx */}
+      {children} 
+      <footer className='py-10 flex justify-center'>
+        Hecho con amor por vercel
+      </footer>
+     
+      </body> 
     </html>
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,6 +10,8 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+
+
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
@@ -18,8 +20,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
